a3ba5b177ea36aa958362b3d295c280f111e6cb3
ffwc0909 <71242654+ffwc0909@users.noreply.github.com>
2020-09-14 08:24:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCngnLpGOQ82PLo6WBGjl3jvTb0_1SCM2I",
+  authDomain: "ffwc-64560.firebaseapp.com",
+  databaseURL: "https://ffwc-64560.firebaseio.com",
+  projectId: "ffwc-64560",
+  storageBucket: "ffwc-64560.appspot.com",
+  messagingSenderId: "104678253283",
+  appId: "1:104678253283:web:33c623bce7eb545d18dbd6",
+  measurementId: "G-8MWSSZ2J1H"
 };
 
      
